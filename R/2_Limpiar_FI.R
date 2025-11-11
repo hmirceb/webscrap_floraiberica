77619c57699524ebfc96f049e58aecfcb9a58f6e
hmirceb <hectorm94@gmail.com>
2025-11-11 10:49:16 +0100

    Update

--- R/2_Limpiar_FI.R
+++ R/2_Limpiar_FI.R
@@ -30,78 +30,122 @@ fi_names_temp <- fi_names_temp %>%
                                          grepl("\\bvar\\.", taxa) ~ "variedad",
                                          grepl("\\bsubvar\\.", taxa) ~ "subvariedad",
                                          grepl("\\bsubsp\\.", taxa) ~ "subespecie",
-                                     genero == accepted_taxa ~ "genus",
-                                     .default = "species")) %>%
-  mutate(taxonomic_level = case_when(grepl(" x ", taxa) ~ "hybrid",
+                                     genero == accepted_taxa ~ "genero",
+                                     .default = "especie")) %>%
+  mutate(taxonomic_level = case_when(grepl(" x ", taxa) ~ "hibrido",
                                      .default = taxonomic_level))
 
 # Use eidos_clean_names()
 fi_names_temp <- fi_names_temp %>%
   mutate(taxa2 = sapply(taxa, eidos_clean_names))
 
+# Separate clean name into words and remove those words from full name
+# to get the authority
 fi_names_temp2 <- fi_names_temp %>%
   separate(taxa2, into = c("a", "b", "c"), sep = " ", remove = FALSE) %>%
-  mutate(authorship = trimws(str_replace_all(pattern = a,
+  mutate(taxonomic_level = case_when(grepl("\\[\\?\\]", taxa) ~ "[?]",
+                                     .default = taxonomic_level)) %>%
+  mutate(authority = trimws(str_replace_all(pattern = a,
                                  replacement = "",
                                  string = taxa))) %>%
-  mutate(authorship = ifelse(taxonomic_level != "genus",
+  mutate(authority = ifelse(taxonomic_level != "genero",
                         str_replace_all(pattern = b,
                                         replacement = "",
-                                        string = authorship),
-                        authorship)) %>%
-  mutate(authorship = ifelse(taxonomic_level %in% c("subespecie", "variedad", "forma"),
+                                        string = authority),
+                        authority)) %>%
+  mutate(authority = ifelse(taxonomic_level %in% c("subespecie", "variedad", "forma", "[?]"),
                         str_replace_all(pattern = c,
                                         replacement = "",
-                                        string = authorship),
-                        authorship)) %>%
-  mutate(authorship = str_replace_all(authorship,
+                                        string = authority),
+                        authority)) %>%
+  mutate(authority = str_replace_all(authority,
                                  "f.",
                                  "")) %>%
-  mutate(authorship = str_replace_all(authorship,
+  mutate(authority = str_replace_all(authority,
                                  "var.",
                                  "")) %>%
-  mutate(authorship = str_replace_all(authorship,
+  mutate(authority = str_replace_all(authority,
                                  "subsp.",
                                  "")) %>%
-  mutate(authorship = trimws(eidos_clean_whitespaces(authorship)))
+  mutate(authority = str_remove_all(authority, "\\[\\?\\]")) %>%
+  mutate(authority = trimws(eidos_clean_whitespaces(authority)))
 
+# Remove authority for hybrids and create new column with name
+# and form
 fi_names_temp2 <- fi_names_temp2 %>%
-  mutate(authorship = case_when(taxonomic_level == "hybrid" ~ NA,
-                           authorship == "" ~ NA,
-                           .default = authorship)) %>%
+  mutate(authority = case_when(taxonomic_level == "hibrido" ~ NA,
+                           authority == "" ~ NA,
+                           .default = authority)) %>%
   mutate(taxa4 = case_when(taxonomic_level == "forma" ~ paste(a, b, "f.", c),
                            taxonomic_level == "variedad" ~ paste(a, b, "var.", c),
                            taxonomic_level == "subespecie" ~ paste(a, b, "subsp.", c),
                            .default = taxa2)) %>%
   dplyr::select(-c(a, b, c))
 
+# Turn synonym type color codes into words
 fi_names_temp2 <- fi_names_temp2 %>%
   mutate(synonym_type = case_when(synonym == "#008000" ~ "heterotipico",
                                   synonym == "#800000" ~ "homotipico",
                                   .default = "aceptado")) %>%
   mutate(taxa = eidos_clean_whitespaces(taxa))
 
+# Genera in hybrids appears as A. instead of the full name,
+# fix that:
 fi_names_temp2 <- fi_names_temp2 %>%
-  mutate(hyb_gen = paste0(str_sub_all(genero, 1, 1), "\\.")) %>%
+  mutate(hyb_gen = ifelse(taxonomic_level == "hibrido",
+                          str_split_i(taxa, pattern = " ", i = 1),
+                          "Aknkdfigjdfkg")) %>% # random word to avoid substituting good genera
   mutate(across(c(accepted_taxa, taxa, taxa2, taxa4),
                 \(x) str_replace_all(x,
                                      pattern = hyb_gen,
-                                     replacement = genero)))
+                                     replacement = genero))) %>%
+  mutate(taxa2 = ifelse(taxonomic_level == "hibrido",
+                        taxa,
+                        taxa2),
+         taxa4 = ifelse(taxonomic_level == "hibrido",
+                        taxa,
+                        taxa4))
+
+# Remove abbreviations from canonical names
+fi_names_temp2 <- fi_names_temp2 %>%
+  mutate(taxa2 = str_replace_all(taxa2,
+                                      " f.",
+                                      "")) %>%
+  mutate(taxa2 = str_replace_all(taxa2,
+                                      " var.",
+                                      "")) %>%
+  mutate(taxa2 = str_replace_all(taxa2,
+                                      " subsp.",
+                                      "")) %>%
+  mutate(accepted_taxa = str_replace_all(accepted_taxa,
+                                 " f.",
+                                 "")) %>%
+  mutate(accepted_taxa = str_replace_all(accepted_taxa,
+                                 " var.",
+                                 "")) %>%
+  mutate(accepted_taxa = str_replace_all(accepted_taxa,
+                                 " subsp.",
+                                 ""))
 
 ### FALTA
 # revisar las que salen sin autoria pero si que la tienen
-# las que tienen esto: [?] porque podrian lo que va depsues aparece en la autoria y no deberia
 # sacar informacion taxonomica superior de los generos from WFO o POWO,
 # incluyendo los generos que no estan completos en FI y si familia es NA
 
 
 FI_clean <- fi_names_temp2 %>%
-  dplyr::select(familia, genero, taxa, taxa4, authorship, taxa2,
-                taxonomic_level, accepted_taxa, synonym_type) %>%
-  rename(name = taxa4,
-         full_name = taxa,
-         canonical_name = taxa2,
-         accepted_name = accepted_taxa)
+  dplyr::select(familia, genero, taxonomic_level, taxa, taxa4, authority, taxa2,
+                accepted_taxa, synonym_type) %>%
+  rename(nombre = taxa4,
+         nivel_taxonomico = taxonomic_level,
+         nombre_completo = taxa,
+         nombre_canonico = taxa2,
+         nombre_aceptado = accepted_taxa,
+         tipo_sinonimo = synonym_type,
+         autoridad = authority) %>%
+  # Remove some NA in strings and wierd symbols
+  mutate(across(everything(), \(x) trimws(str_remove_all(x, "NA")))) %>%
+  mutate(across(everything(), \(x) trimws(str_remove_all(x, "€"))))
 
 # Write CSV
 write.csv(FI_clean,
--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Webscrapping Flora Ibérica
 
-A small project to scrape the web of [Flora Iberica](http://www.floraiberica.es/) for synonyms.
+A small project to scrap the web of [Flora Iberica](http://www.floraiberica.es/) for synonyms. This allows an easier or more automated way of looking up several taxa instead of using the search option in Flora Iberica.
 
-Script 1 parses the HTML code of each genus available in the web. Script 2 formats the names into a usable data frame. The "utils" script contains several auxiliary functions.
+The R folder contains several scripts:
+  - Script 1 parses the HTML code of each genus available in the web. 
+  - Script 2 formats the names into a usable data frame. 
+  - The "utils" script contains several auxiliary functions.
 
-All data are accesible under license [CC BY-NC-SA 2.5 ES](https://creativecommons.org/licenses/by-nc-sa/2.5/es/)
+All data are accesible under the license used by Flora Iberica: [CC BY-NC-SA 2.5 ES](https://creativecommons.org/licenses/by-nc-sa/2.5/es/)
 
 The proper citation for this dataset and Flora Iberica as a whole is:
 
